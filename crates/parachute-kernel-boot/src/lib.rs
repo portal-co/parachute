0857f7d47683ca295116a88632626c21b4ed3c9e
gkgoat <65402989+gkgoat1@users.noreply.github.com>
2026-02-11 01:52:17 -0500

    riscv

--- crates/parachute-kernel-boot/src/lib.rs
+++ crates/parachute-kernel-boot/src/lib.rs
@@ -1,1 +1,15 @@
 #![no_std]
+#![cfg_attr(feature = "allocator-api", feature(allocator_api))]
+
+use spin::Mutex;
+#[cfg(feature = "alloc")]
+extern crate alloc;
+
+pub struct BootMem(Mutex<BootMemInternal>);
+enum BootMemInternal{
+    #[cfg(feature = "limine")]
+    DormantLimine{
+        memmap: &'static limine::request::MemoryMapRequest,
+    },
+    
+}
